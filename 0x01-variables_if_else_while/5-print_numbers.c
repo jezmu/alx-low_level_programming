8b5e11c13ea8755d51e08c88d6051fb162802e8f
jezmu <jezmuenterprises@gmail.com>
2022-03-14 15:13:20 -0700

    variable

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d = 0;
+
+	for (; d != 10; d++)
+		printf("%d", d);
+	printf("\n");
+	return (0);
+}
+
