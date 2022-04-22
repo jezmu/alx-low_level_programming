4b5c188c9623e184c1900ad7c25dc9fe98c9945a
jezmu <jezmuenterprises@gmail.com>
2022-04-21 17:34:11 -0700

    list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef _LISTS_H_
-#define _LISTS_H_
+#ifndef _LISTS_
+#define _LISTS_
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,11 +21,10 @@ typedef struct list_s
     struct list_s *next;
 } list_t;
 
-int _strlen(const char *s);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-void startup(void) __attribute__((constructor));
+
 #endif
