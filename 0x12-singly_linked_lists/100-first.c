25536613e703607e761235ce8cdd5ff71e15587e
jezmu <jezmuenterprises@gmail.com>
2022-04-21 18:25:36 -0700

    commit

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,12 @@
-#include "lists.h"
+#include <stdio.h>
+
 /**
- * text - prints to stdout, runs before main
- *
- * Return: Nothing
+ * bmain - function executed before main
+ * Return: no return.
  */
-void text(void)
+
+void __attribute__ ((constructor)) bmain()
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
 }
