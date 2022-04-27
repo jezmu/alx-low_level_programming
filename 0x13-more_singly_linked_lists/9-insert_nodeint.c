7cb37cff769c67d1dc3a41b5b8ed63441d61add4
jezmu <jezmuenterprises@gmail.com>
2022-04-26 19:17:54 -0700

    commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,45 +1,41 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a new node in a linked list,
- * at a given position
- * @head: pointer to the first node in the list
- * @idx: index where the new node is added
- * @n: data to insert in the new node
+ * insert_nodeint_at_index - inserts a new node at a given position.
+ * @head: the pointer to the starting node of the listint_t list
+ * @idx: the position where the new node is to be inserted
+ * @n: the data to be stored in the new node
  *
- * Return: pointer to the new node, or NULL
+ * Return: the address of the new node, or NULL if it failed
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-    unsigned int i;
-    listint_t *new;
-    listint_t *temp = *head;
+	listint_t *new, *temp = *head;
+	unsigned int i = 0;
 
-    new = malloc(sizeof(listint_t));
-    if (!new || !head)
-        return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
 
-    new->n = n;
-    new->next = NULL;
+	new->n = n;
 
-    if (idx == 0)
-    {
-        new->next = *head;
-        *head = new;
-        return (new);
-    }
+	if (!idx)
+	{
+		new->next = temp;
+		*head = new;
+		return (new);
+	}
 
-    for (i = 0; temp && i < idx; i++)
-    {
-        if (i == idx - 1)
-        {
-            new->next = temp->next;
-            temp->next = new;
-            return (new);
-        }
-        else
-            temp = temp->next;
-    }
+	while (i++ < (idx - 1))
+	{
+		if (!temp || !(temp->next))
+			return (NULL);
 
-    return (NULL);
+		temp = temp->next;
+	}
+
+	new->next = temp->next;
+	temp->next = new;
+
+	return (new);
 }
